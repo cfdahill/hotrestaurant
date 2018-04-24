24a7880d9b8c67c24c21d88e04fc00278b18a00f
unknown <webd3vdave@gmail.com>
2018-04-23 19:07:30 -0700

    pseudo code

---
+++ code.js
@@ -0,0 +1,27 @@
+// Dependencies
+// ===========================================================
+var express = require("express");
+//body parser
+var app = express();
+var PORT = 3000;
+/// array for reservations, similar one for wait list
+var reservations = 
+[{
+    customerName: "",
+    customerEmail: "",
+    customerID: "",
+    phoneNumber: "",
+
+}];
+
+//constructor for building 'reservation' 
+
+//start server & listen for URL hit
+
+//paths to different pages
+
+//listen for POST from page forms
+
+//listen for GET for view tables request
+
+//function to clear the reservation/wait list arrays
